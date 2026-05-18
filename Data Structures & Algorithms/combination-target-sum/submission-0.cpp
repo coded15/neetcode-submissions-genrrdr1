35e9cf13ac656d1011226bd8e2aedb0461623545
Ichchha Gupta <91874598+coded15@users.noreply.github.com>
2026-05-18 18:49:12 +0530

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        map<vector<int>,int> result;
+        vector<int> combination;
+        dfs(0, target, nums, combination, result);   
+        vector<vector<int>> uniqueResult;
+        for(auto &it: result)
+        {
+            uniqueResult.push_back(it.first);
+        }
+        return uniqueResult;
+    }
+
+    void dfs(int i, int target, vector<int>& nums, vector<int> &combination,map<vector<int>,int> &result)
+    {
+        if(target == 0)
+        {
+            result[combination]++;
+            return;
+        }
+        if(target < 0 ||i>=nums.size())
+        {
+            return;
+        }
+
+        if (nums[i] <= target) {
+            combination.push_back(nums[i]);
+
+            dfs(i, target - nums[i], nums, combination, result);
+
+            combination.pop_back(); // move inside
+        }
+
+
+        dfs(i+1, target, nums, combination, result);
+    }
+};
