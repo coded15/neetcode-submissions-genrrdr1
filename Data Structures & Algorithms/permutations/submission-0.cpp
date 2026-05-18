25dc93ac871cf2d727a47f5523346d2e90eacb93
Ichchha Gupta <91874598+coded15@users.noreply.github.com>
2026-05-18 20:36:30 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+    vector<vector<int>> result;
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<bool> seen(nums.size(), false);
+        vector<int> current;
+        permutation(nums, current, seen);
+        return result;
+    }
+
+    void permutation(vector<int>& nums, vector<int>& current, vector<bool>& seen) {
+        if (current.size() == nums.size()) {
+            result.push_back(current);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (!seen[i]) {
+                current.push_back(nums[i]);
+                seen[i] = true;
+                permutation(nums, current, seen);
+                current.pop_back();
+                seen[i] = false;
+                // permutation(nums, current, seen);
+            }
+        }
+    }
+};
