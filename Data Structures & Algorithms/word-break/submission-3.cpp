44825bda85f826de591bdf1e32e87657a0ed9f99
Ichchha Gupta <91874598+coded15@users.noreply.github.com>
2026-08-01 23:58:32 +0530

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.cpp
@@ -0,0 +1,44 @@
+class Solution {
+   public:
+    unordered_map<int, bool> memo;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        memo[s.length()] = true;
+        return dfs(s, wordDict, 0);
+    }
+
+    bool dfs(const string& s, const vector<string>& wordSet, int i) {
+        // if (i == s.size()) {
+        //     return true;
+        // }
+        if (memo.find(i) != memo.end()) {
+            return memo[i];
+        }
+
+        // for (int j = i; j < s.size(); j++) {
+        //     if (wordSet.find(s.substr(i, j - i + 1)) != wordSet.end()) {
+        //         if (dfs(s, wordSet, j + 1)) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        for (const string& word : wordSet) {
+            if (((i + word.length()) <= s.length()) && (s.substr(i, word.length()) == word)
+            // don't forget to have an equality with the length of string s, while comparing it with i + word.length(), as we have set memo[s.length()] as true
+            // dont use "i + word.length()", in  (s.substr(i, i + word.length()) == word) just use word.length
+                // (memo.find(i + word.size()) != memo.end())
+                // in each iteration, a true result should be decided by whether the current word is
+                // matching the substring not whether its memo is true, because thats we are trying
+                // to construct, not the other way around
+            ) {
+                if (dfs(s, wordSet, i + word.length())) {
+                    // memo[i + word.length()] = true;
+                    memo[i] = true;
+                    return true;
+                }
+            }
+        }
+        memo[i] = false; // you will have to explicitly mark this as false, as this is a map, and it doesn't have s.length() entries inserted and intialized to false by default, we are inserting keys step by setp as we compute.
+        return false;
+    }
+};
