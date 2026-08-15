73139c6df10322c2c740acb964d9b5fe6aaad05c
Ichchha Gupta <91874598+coded15@users.noreply.github.com>
2026-08-15 16:01:51 +0530

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.cpp
@@ -0,0 +1,44 @@
+class Solution {
+   public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> allowedWords;
+        for (auto it : wordList) {
+            allowedWords.insert(it);
+        }
+        if (!allowedWords.count(endWord) || (beginWord.length() != endWord.length())) {
+            return 0;
+        }
+        queue<string> matchedWords;
+        matchedWords.push(beginWord);
+        allowedWords.erase(beginWord);
+        return bfs(endWord, allowedWords, matchedWords);
+    }
+
+    int bfs(string endWord, unordered_set<string>& allowedWords, queue<string>& matchedWords) {
+        int result = 0;
+        while (!matchedWords.empty()) {
+            int levelSize = 0;
+            levelSize = matchedWords.size();
+            while (levelSize--) {
+                string currentWord = matchedWords.front();
+                matchedWords.pop();
+                if (currentWord == endWord) {
+                    return ++result;
+                }
+                for (int i = 0; i < currentWord.size(); i++) {
+                    for (char j = 'a'; j <= 'z'; j++) {
+                        string temp = currentWord;
+                        temp[i] = j;
+                        if (allowedWords.find(temp) != allowedWords.end()) {
+                            matchedWords.push(temp);
+                            allowedWords.erase(temp);
+                        }
+                    }
+                    // cout << temp << " ";
+                }
+            }
+            result++;
+        }
+        return 0;
+    }
+};
